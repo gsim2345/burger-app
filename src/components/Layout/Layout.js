dfd74b29127e1467f436f4c8607622a2dc061118
gsim2345 <gb@gabriellasimon.me>
2019-02-04 17:54:36 +0100

    Added css-modules style to Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 //import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     // needs a wrapper root element , a div, or a hoc (Aux), (can return only array or wrapped componenets)
     <>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </>
