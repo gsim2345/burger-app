a7351282eac213a978047304433120f40c793887
gsim2345 <gb@gabriellasimon.me>
2019-02-09 20:10:20 +0100

    Added a toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 //import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     // needs a wrapper root element , a div, or a hoc (Aux), (can return only array or wrapped componenets)
     <>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
