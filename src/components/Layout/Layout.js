3801471369843be44505bbbd0c248e6375b0e490
gsim2345 <gb@gabriellasimon.me>
2019-02-10 22:00:22 +0100

    Added sidedrawer for mobile devices

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,14 @@ import React from 'react';
 //import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 
 const layout = (props) => (
     // needs a wrapper root element , a div, or a hoc (Aux), (can return only array or wrapped componenets)
     <>
         <Toolbar />
+        <SideDrawer/>
         <main className={classes.Content}>
             {props.children}
         </main>
