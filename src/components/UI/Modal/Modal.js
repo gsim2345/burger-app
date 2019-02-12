491bdbb164d964075d357f2bdedc17141fd09618
gsim2345 <gb@gabriellasimon.me>
2019-02-12 21:25:33 +0100

    turned modal to class component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.module.css';
 //import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                // -100vh - viewport height. Will slide off the screen if not show
-                opacity: props.show ? '1' : '0'
-            }}   >
-            {props.children}
-        </div>
-    </>
-);
+// turning modal component to class component, to be able to implement shouldComponentUpdate
+class Modal extends Component {
+    render() {
+        return (
+            <>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        // -100vh - viewport height. Will slide off the screen if not show
+                        opacity: this.props.show ? '1' : '0'
+                    }}   >
+                    {this.props.children}
+                </div>
+            </>
+        )
+    }
+}
 
-export default modal;
+export default Modal;
