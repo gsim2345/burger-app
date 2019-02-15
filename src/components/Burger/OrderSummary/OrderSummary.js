8fbc0748fd88205b2bf7a1e7c2f63192fb9e2e18
gsim2345 <gb@gabriellasimon.me>
2019-02-15 16:54:34 +0100

    Adjusted folder structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 
 import React, {Component} from 'react';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
 // turn into Class component, so we can add lifecycle hooks, and to see when it's updated
