cf91a99c0a62595dee67fa01ddef31a6bffb4723
gsim2345 <gb@gabriellasimon.me>
2019-03-13 15:23:45 +0100

    Added Checkout Container and CheckoutSummary component

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Checkout extends Component {
+
+    // summary with price
+    // burger itself
+    // button to cancel and go back
+    // continue button => load form
+
+}
+
+export default Checkout;
