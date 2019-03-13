1f0da3c85aacef776c382cedea780550f568c78b
gsim2345 <gb@gabriellasimon.me>
2019-03-13 15:53:39 +0100

    Added Checkout component and display on page for testing

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
 
@@ -6,6 +7,24 @@ class Checkout extends Component {
     // burger itself
     // button to cancel and go back
     // continue button => load form
+
+    // dummy data for now
+    state= {
+        ingredients: {
+            salad: 1,
+            meat: 1,
+            cheese: 1,
+            bacon: 1
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <CheckoutSummary ingredients={this.state.ingredients}/>
+            </div>
+        );
+    }
 
 }
 
