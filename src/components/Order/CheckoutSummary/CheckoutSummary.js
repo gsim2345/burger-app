79b00fcc1447b1e09a320ebea02ca7694dd460ed
gsim2345 <gb@gabriellasimon.me>
2019-03-13 15:29:40 +0100

    Added CSS to CheckoutSummary component

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Burger from '../../Burger/Burger';
 import Button from '../../UI/Button/Button';
+import classes from './checkoutSummary.module.css'
 
 const checkoutSummary = (props) => {
     return (
-        <div>
+        <div className={classes.CheckoutSummary}>
             <h1>We hope it tastes good!</h1>
             <div style={{width: '300px', height: '300px', margin: "auto"}}>
                 <Burger ingredients={props.ingredients}/>
