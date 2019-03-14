f7cd1abb92710341cbbcd8bca7d297b0cc617bef
gsim2345 <gb@gabriellasimon.me>
2019-03-14 19:22:35 +0100

    changed navigationItems to new name and link

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,8 +5,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = (props) => (
     // active is the same as active={true}
     <ul className={classes.NavigationItems}>
-        <NavigationItem link='/' active>Burger Builder</NavigationItem>
-        <NavigationItem link='/'>Checkout</NavigationItem>
+        <NavigationItem link='/' exact>Burger Builder</NavigationItem>
+        <NavigationItem link='/orders'>Orders</NavigationItem>
     </ul>
 );
 
