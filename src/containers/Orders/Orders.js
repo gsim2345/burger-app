5408d8d8c0dc6a09e6c543e2f0e540b177818a76
gsim2345 <gb@gabriellasimon.me>
2019-03-15 15:15:29 +0100

    Fetching orders from firebase in Orders component

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+
 
 class Orders extends Component {
     // we start with deleting all orders in firebase, and changed all ingredients to 0
+
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('orders.json')
+            .then(res => {
+                console.log(res.data);
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    // we are pushing an object, with the copy of data, and add key manually
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                console.log(fetchedOrders);
+                this.setState({loading: false, orders: fetchedOrders});
+            })
+            .catch(error => {
+
+            });
+    }
 
     render() {
         return(
