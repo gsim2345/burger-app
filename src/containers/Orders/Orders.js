b14b15913096a8c3ffb03408f02ca168f340151f
gsim2345 <gb@gabriellasimon.me>
2019-03-16 09:57:28 +0100

    Output orders from data from firebase

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -35,8 +35,12 @@ class Orders extends Component {
     render() {
         return(
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order key={order.id}
+                    ingredients={order.ingredients}
+                    price={+order.price}/>
+                    // you can convert to number with +
+                ))}
             </div>
         );
     }
