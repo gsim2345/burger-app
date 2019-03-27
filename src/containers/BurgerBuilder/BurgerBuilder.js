e689200333a47cfb25cb8cd33703feb22ac3a67b
gsim2345 <gb@gabriellasimon.me>
2019-03-27 13:48:46 +0100

    purchasable not updating from state anymore - fix order button enable-disable

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,7 +20,6 @@ class BurgerBuilder extends Component {
         // we move over ingredients and totalPrice to the reducer
        
         // the following: local UI states (showing modals, error messages, disable/enable button , etc) Not neceserraly needs Redux
-        purchasable: false,
         purchasing:false,
         loading: false, 
         error: false
@@ -43,6 +42,7 @@ class BurgerBuilder extends Component {
         */
     }
 
+    // need to update the purchasable prop whenever our ingredients change
     updatePurchaseState = (ingredients) => {
         
         const sum = Object.keys(ingredients).map(igKey => {
@@ -51,8 +51,8 @@ class BurgerBuilder extends Component {
         .reduce((sum, el) => {
             return sum + el;
         },0);
-
-        this.setState({purchasable: sum > 0});
+        // we don't update the state anymore, just return the result of checking the sum. 
+        return sum > 0
     } 
 
     purchaseHandler = () => {
@@ -106,7 +106,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.props.onIngredientAdded}
                     ingredientRemoved={this.props.onIngredientRemoved}
                     disabled={disabledInfo}
-                    purchasable={this.state.purchasable}
+                    purchasable={this.updatePurchaseState(this.props.ings)}
                     ordered={this.purchaseHandler}
                     price={this.props.price}
                     />
