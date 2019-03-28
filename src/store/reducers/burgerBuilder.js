193d1ad6aa8813dbc8baa008174f809ab5983e93
gsim2345 <gb@gabriellasimon.me>
2019-03-28 19:06:40 +0100

    reorganize the folder structure in the store

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,4 @@
-import * as actionTypes from './actions.js';
+import * as actionTypes from '../actions/actionTypes.js/index.js';
 
 const initialState = {
     // temporarily we set ingredients without the ajax request
