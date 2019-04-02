16c91fb5dc7d6eeb8bcf9e8d04a019e806cea7b6
gsim2345 <gb@gabriellasimon.me>
2019-04-02 21:52:54 +0200

    Added redirect to Checkout page in case reload

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
@@ -17,18 +17,25 @@ class Checkout extends Component {
 
 
     render() {
-        return (
-            <div>
-                <CheckoutSummary 
-                ingredients={this.props.ings}
-                checkoutCancelled={this.checkoutCancelledHandler}
-                checkoutContinued={this.checkoutContinuedHandler}/>
-                <Route 
-                    path={this.props.match.path + '/contact-data'} 
-                    component={ContactData} />
-                {/* we no longer need pass in props, that will go through Redux, so we simply add as component, and not render as JSX*/}
-            </div>
-        );
+        // if ingredients are not yet loaded, we get an error message
+        // we redirect to burger builder page, where there is a loader until no ingredients are loaded. 
+        let summary = <Redirect to='/'/>;
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary 
+                    ingredients={this.props.ings}
+                    checkoutCancelled={this.checkoutCancelledHandler}
+                    checkoutContinued={this.checkoutContinuedHandler}/>
+                    <Route 
+                        path={this.props.match.path + '/contact-data'} 
+                        component={ContactData} />
+                    {/* we no longer need pass in props, that will go through Redux, so we simply add as component, and not render as JSX*/}
+                </div>
+            )
+        }
+
+        return summary;
     }
 }
 
