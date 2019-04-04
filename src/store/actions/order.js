e36c38466f4cd104e51199151a9d202bf6b2e06e
gsim2345 <gb@gabriellasimon.me>
2019-04-04 18:40:32 +0200

    Fetch orders via  Redux

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -44,8 +44,54 @@ export const purchaseBurger = (orderData) => {
     }
 }
 
+
 export const purchaseInit = () => {
     return {
         type: actionTypes.PURCHASE_INIT
     }
+}
+
+// Orders
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS, 
+        orders: orders
+    }
+}
+
+export const fetchOrdersFail = (error) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAIL,
+        error: error
+    }
+}
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    }
+}
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart());
+        axios.get('orders.json')
+            .then(res => {
+                
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    // we are pushing an object, with the copy of data, and add key manually
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                dispatch(fetchOrdersSuccess(fetchedOrders));
+            })
+            .catch(error => {
+                dispatch(fetchOrdersFail(error));
+            });
+    }
+    
 }
