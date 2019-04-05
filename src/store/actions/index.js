45a5b0f20e735e309ea48ad64ae256eca28e1f52
gsim2345 <gb@gabriellasimon.me>
2019-04-05 14:13:06 +0200

    Added new actions - Auth

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,4 +2,6 @@ export {addIngredient, removeIngredient, initIngredients} from './burgerBuilder'
 
 export {purchaseBurger, purchaseInit, fetchOrders} from './order';
 
+export { auth } from './auth';
+
 
