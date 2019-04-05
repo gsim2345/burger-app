a3831d5f1effe5bca71b914e04a525ec4370bfab
gsim2345 <gb@gabriellasimon.me>
2019-04-05 12:23:38 +0200

    Set up route for Auth

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders.js';
+import Auth from '.containers/Auth/Auth';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={BurgerBuilder} />
             <Route path="/orders" exact component={Orders} />
+            <Route path="/auth" exact component={Auth} />
             <Route path="/checkout" component={Checkout} />
           </Switch>
         </Layout>
