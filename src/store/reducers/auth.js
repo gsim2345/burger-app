010b3481fdfce83389cc1d726b3fd0477fb4e0c7
gsim2345 <gb@gabriellasimon.me>
2019-04-08 10:33:02 +0200

    Set reducer state to initialState

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -8,6 +8,17 @@ const initialState = {
     loading: false
 }
 
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case actionTypes.AUTH_START: return authStart(state, action);
+        case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
+        case actionTypes.AUTH_FAIL: return authFail(state, action);
+        default: return state
+    }
+    
+}
+
 const authStart = (state, action) => {
     return updateObject(state, { error: null, loading: true });
 }
@@ -28,14 +39,5 @@ const authFail = (state, action) => {
     });
 }
 
-const reducer = (state, action) => {
-    switch(action.type) {
-        case actionTypes.AUTH_START: return authStart(state, action);
-        case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
-        case actionTypes.AUTH_FAIL: return authFail(state, action);
-        default: return state
-    }
-    
-}
 
 export default reducer;
