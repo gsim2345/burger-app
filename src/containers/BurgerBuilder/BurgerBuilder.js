0c3d20f67b53505c9844ae88a17deb31a2e5ceeb
gsim2345 <gb@gabriellasimon.me>
2019-04-08 22:14:21 +0200

    Order button shows different messages if logged in

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,6 +94,7 @@ class BurgerBuilder extends Component {
                     disabled={disabledInfo}
                     purchasable={this.updatePurchaseState(this.props.ings)}
                     ordered={this.purchaseHandler}
+                    isAuth={this.props.isAuthenticated}
                     price={this.props.price}
                     />
                 </>
@@ -127,7 +128,8 @@ const mapStateToProps = state => {
     return { 
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !== null
     };
 }
 
