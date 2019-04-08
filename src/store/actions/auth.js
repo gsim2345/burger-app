0cb622b9d8ec7dbe083ed435a36358ca07bb5228
gsim2345 <gb@gabriellasimon.me>
2019-04-08 10:54:58 +0200

    store the token in the state

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
     }
 }
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        idToken: token,
+        userId: userId
     }
 }
 
@@ -38,7 +39,10 @@ export const auth = (email, password, isSignup) => {
         axios.post(url, authData)
         .then(response => {
             console.log(response);
-            dispatch(authSuccess(response.data));
+            // localId: userId
+            // idToken: the token 
+            // refreshToken needs if we don't want the login to expire in an hour. 
+            dispatch(authSuccess(response.data.idToken, response.data.localId));
         })
         .catch(error => {
             console.log(error);
